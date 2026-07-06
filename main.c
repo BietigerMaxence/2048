b5000a9dd72300ce0cd3034e498366ba82cc186d
bietigerm <maxence.bietiger@efrei.net>
2026-07-06 17:03:49 +0200

    Ajout de la fonction merge_left

--- main.c
+++ main.c
@@ -60,21 +60,49 @@ void move_left(int a_board[SIZE][SIZE]) {
     }
 }
 
+void merge_left(int a_board[SIZE][SIZE]) {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE - 2; j++) {
+            if (a_board[i][j] == a_board[i][j+1] && a_board[i][j] != 0) {
+                a_board[i][j] += a_board[i][j+1];
+                a_board[i][j+1] = 0;
+
+            }
+        }
+    }
+}
+
 int main() {
     srand(time(NULL));
     int board[SIZE][SIZE] = {
-        {0, 2, 0, 4},
-        {0, 0, 2, 0},
-        {4, 0, 0, 2},
-        {0, 0, 0, 8}
+        {0, 2, 2, 2}, // Attendu 4, 2, 0, 0
+        {2, 0, 0, 2}, // Attendu 4, 0, 0, 0
+        {2, 2, 2, 2}, // Attendu 4, 4, 0, 0
+        {4, 4, 8, 8}  // Attendu 8, 16, 0, 0
     };
 
+    int board1[SIZE][SIZE] = {
+        {2, 2, 4, 0}, // Attendu 4, 4, 0, 0
+        {4, 4, 4, 0}, // Attendu 8, 4, 0, 0
+        {2, 4, 4, 4}, // Attendu 2, 8, 4, 0
+        {0, 0, 0, 0}  // Attendu 0, 0 0, 0
+    };
 
     //init_board(board);
     //spawn_tile(board);
     //spawn_tile(board);
 
+    printf("board\n");
     print_board(board);
     move_left(board);
+    merge_left(board);
+    move_left(board);
     print_board(board);
+
+    printf("board1\n");
+    print_board(board1);
+    move_left(board1);
+    merge_left(board1);
+    move_left(board1);
+    print_board(board1);
 }
